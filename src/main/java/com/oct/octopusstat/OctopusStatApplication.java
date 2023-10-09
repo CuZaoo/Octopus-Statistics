3ce58379914c6fc7088008110375f703ade5846f
CuZaoo <870619807@qq.com>
2023-10-09 21:32:07 +0800

    SpringBoot模板

---
+++ src/main/java/com/oct/octopusstat/OctopusStatApplication.java
@@ -0,0 +1,13 @@
+package com.oct.octopusstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctopusStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OctopusStatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oct/octopusstat/OctopusStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oct.octopusstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
